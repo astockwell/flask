31005b2229bee12fdee007ade7029b4a6007117e
Alex Stockwell <astockwell@gmail.com>
2014-11-30 10:26:28 -0800

    added inline style class example use

--- lib/InlineStyler.class.php
+++ lib/InlineStyler.class.php
@@ -3,6 +3,16 @@
  * Inline Styler
  *
  * Generates style attributes for use inline within HTML tags
+ *
+ * Usage:
+ *	<?php
+ *		$theme_dir = get_bloginfo('template_directory');
+ *		$styles = new InlineStyler;
+ *		$background_pattern = get_field('background_pattern'); if ($background_pattern):
+ *			$styles->add("background-image: url('$theme_dir/img/patterns/$background_pattern');");
+ *		endif;
+ *	?>
+ *	<section class="body__wrapper" role="main" <?php echo $styles->write(); ?>>
  */
 class InlineStyler
 {
